b40db53b275eff1fc0c71928765ba0a123d18ad8
maxdilger <max@dilger.ca>
2017-05-25 13:02:43 -0600

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,288 @@
+package GameData;
+import java.util.ArrayList;
+import java.lang.Math;
+import java.util.Random;
+
+//so many imports /:
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class Game extends Application {
+	//dimensions of the game
+	private final int width = 1216;
+	private final int height = 768;
+	int[][] PixelArray = new int[width][height];
+	private final int maxX = width - 1;
+	private final int maxY = height - 1;
+	private final int fps = 45; // 55 is about the fastest atm //50 is the normal amount
+	double prevTime = 0;
+	int prefferedTime = (1000/fps);
+	int sleepTime = 0;
+	
+	public double mouseX = 0;
+	public double mouseY = 0;
+	
+	Random RandInt = new Random();
+	int number = 0;
+	int pressTimer = 0;
+	int shootDelay = 12;
+	int spawnTimer = 0;
+	int spawnDelay = 150;
+	//int myRandomNumber = 0;
+	//myRandomNumber = r.nextInt(maxValue-minValue+1)+minValue;
+	String button = "";
+	public static ArrayList<Objects> AiList = new ArrayList<Objects>();
+	public static ArrayList<Objects> Remove = new ArrayList<Objects>();
+	
+	public void spawn(){
+		int sides = RandInt.nextInt(6-1+1)+1;
+		int amount = RandInt.nextInt(10-2+1)+2;
+		
+		if (sides == 1){
+			for (int i = 0; i <amount; i++){
+				AiList.add(new Objects(30 + 65*i, 10, 2,"player32x32.png", 32, 32, "", 0, 0));
+			}
+		} else if (sides == 2){
+			for (int i = 0; i <amount; i++){
+				AiList.add(new Objects(800 + 65*i, 10, 2,"player32x32.png", 32, 32, "", 0, 0));
+			}
+		} else if (sides == 3){
+			for (int i = 0; i <amount; i++){
+				AiList.add(new Objects(30 + 65*i, 700, 2,"player32x32.png", 32, 32, "", 0, 0));
+			}
+		} else if (sides == 4){
+			for (int i = 0; i <amount; i++){
+				AiList.add(new Objects(800 + 65*i, 700, 2,"player32x32.png", 32, 32, "", 0, 0));
+			}
+		} else if (sides == 5){
+			for (int i = 0; i <amount; i++){
+				AiList.add(new Objects(30 + 65*i, 10, 2,"player32x32.png", 32, 32, "", 0, 0));
+			}
+			
+			for (int i = 0; i <amount; i++){
+				AiList.add(new Objects(800 + 65*i, 700, 2,"player32x32.png", 32, 32, "", 0, 0));
+			}
+		} else if (sides == 6){
+			for (int i = 0; i <amount; i++){
+				AiList.add(new Objects(800 + 65*i, 10, 2,"player32x32.png", 32, 32, "", 0, 0));
+			}
+			for (int i = 0; i <amount; i++){
+				AiList.add(new Objects(30 + 65*i, 700, 2,"player32x32.png", 32, 32, "", 0, 0));
+			}
+		}
+		
+	}
+
+	
+	public void start(Stage Window) {
+	    Window.setTitle("Game");
+	    System.out.println("test");
+	    Objects player = new Objects(400, 400, 7, "PlayerRight64x64.png", 64, 64, "player", 0, 0);
+	    Objects hpBar = new Objects("Hp64.png");
+	    Image Background = new Image("BackGround1216x768.png");
+	    //Objects object1 = new Objects(800, 700, 4, "player32x32.png", 32);
+	    //Objects object2 = new Objects(64, 400, 4, "player32x32.png", 32);
+	    
+		for (int i = 0; i <2; i++) {
+			AiList.add(new Objects(30 + 72*i, 10, 2,"player32x32.png", 32, 32, "", 0, 0));
+		}
+	    
+	    Group root = new Group();
+	    Scene theWindow = new Scene(root);
+	    Window.setScene(theWindow);
+	    Window.setResizable(false);
+	    
+	    Canvas canvas = new Canvas(width, height);
+	    root.getChildren().add(canvas);
+	    
+	    GraphicsContext screen = canvas.getGraphicsContext2D();
+	    
+	    //array for keyboard input
+	    ArrayList<String> input = new ArrayList<String>();
+	    
+	    
+	    //keyboard input
+        theWindow.setOnKeyPressed(new EventHandler<KeyEvent>() {
+                public void handle(KeyEvent e) {
+                    String code = e.getCode().toString();
+ 
+                    // only add once... prevents duplicates
+                    if ( !input.contains(code) ){
+                        input.add( code );
+                    	button = code;
+                    }
+                }
+            });
+ 
+        theWindow.setOnKeyReleased(new EventHandler<KeyEvent>() {
+                public void handle(KeyEvent e) {
+                    String code = e.getCode().toString();
+                    input.remove( code );
+                    button = "";
+                }
+            });
+        
+        //mouse input
+        theWindow.setOnMouseMoved(new EventHandler<MouseEvent>(){
+        	public void handle(MouseEvent event){
+        		mouseX = event.getX();
+        		mouseY = event.getY();
+        	}
+        });
+	        
+	        // main game loop
+	    new AnimationTimer(){
+	    	public void handle(long NanoSeconds){
+	    		//double seconds = NanoSeconds / 1000000000;
+	    		//System.out.println(time);
+	    		
+	    		//making the player move while within the window
+	    		//left movement and collision detection
+	    		if (input.contains("LEFT") || input.contains("A") && player.Left() > 0) {
+	    			double moveX = Math.min(player.Left(), player.MoveSpeed());
+	    			player.Move(player, -moveX, 0);
+	    			player.Sprite(player,"PlayerLeft64x64.png");
+	    		}
+	    		//right movement and collision detection
+	    		if (input.contains("RIGHT") || input.contains("D") && player.Right() < maxX) {
+	    			double moveX = Math.min(maxX - player.Right(), player.MoveSpeed());
+	    			player.Move(player, moveX, 0);
+	    			player.Sprite(player,"PlayerRight64x64.png");
+	    		}
+	    		//up movement and collision detection
+	    		if (input.contains("UP") || input.contains("W") && player.Top() > 0) {
+	    			double moveY = Math.min(player.Top(), player.MoveSpeed());
+	    			player.Move(player,0, -moveY);
+	    		}
+	    		//down movement and collision detection
+	    		if (input.contains("DOWN") || input.contains("S") && player.Bottom() < maxY) {
+	    			double moveY = Math.min(maxY - player.Bottom(), player.MoveSpeed());
+	    			player.Move(player, 0, moveY);
+	    			//System.out.println("DOWN");
+	    		}
+	    		
+	    		if (input.contains("SPACE")){
+	    			if (pressTimer <= 0){
+    					player.Shoot(player, mouseX, mouseY, 3.5);
+    					pressTimer = shootDelay;
+    				}
+	    		}
+	    		
+	    		/*
+	    		//other buttons
+	    		switch (button){
+	    			case "SPACE":
+	    				if (pressTimer <= 0){
+	    					player.Shoot(player, mouseX, mouseY, 3.5);
+	    					pressTimer = delay;
+	    				}
+	    		}
+	    		
+	    		*/
+	    		//redrawing the screen after player moves
+	    		//screen.clearRect(0, 0, width, height);
+	    		screen.drawImage(Background, 0, 0);
+
+	    		// dealing with objects
+	    		for (Objects blob : AiList){
+
+	    			if (blob.Left() < 0 || blob.Bottom() < 0 || 
+	    				blob.Right() > maxX || blob.Top() > maxY){
+	    					Remove.add(blob);
+	    					System.out.println("removed");
+	    			}
+	    			
+	    			if (blob.Touching() == true){
+	    				switch (blob.Name()){
+	    					case "":
+	    						Remove.add(blob);
+	    						break;
+	    					case "bullet":
+	    						Remove.add(blob);
+	    						break;
+	    					case "player":
+	    						System.out.println("player");
+	    				}
+	    				
+	    			} else if (blob.Name() == "") {
+	    				blob.Follow(blob, player);
+	    				blob.Sprite(blob, "player32x32.png");
+	    			} else if (blob.Name() == "bullet"){
+	    				//System.out.println("test");
+	    				blob.Move(blob, 0, blob.Yspeed());
+	    				blob.Move(blob, blob.Xspeed(), 0);
+	    			}
+	    			screen.drawImage(blob.sprite(), blob.Left(), blob.Top());
+	    		}
+	    		
+	    		for (Objects Delete: Remove){
+	    			AiList.remove(Delete);
+	    			Objects.objectlist.remove(Delete);
+	    			Delete = null;
+	    		}
+	    		
+	    		Remove.clear();
+	    		
+	    		/*
+	    		if (AiList.isEmpty()){
+	    			spawn();
+	    		}
+	    		
+	    		*/
+	    		
+	    		if (spawnTimer <= 0){
+	    			spawn();
+	    			spawnTimer = spawnDelay;
+	    		}
+	    		
+	    		hpBar.Follow(hpBar, player);
+	    		hpBar.Sprite(hpBar, "Hp64.png");
+	    		
+	    		screen.drawImage(player.sprite(), player.Left(), player.Top());
+	    		screen.drawImage(hpBar.sprite(), hpBar.Left(), hpBar.Top());
+	    		//System.out.println(player.Touching());
+	    		
+	    		try {
+	    			double time = NanoSeconds / 1000000;
+	    		    double timetaken =  time - prevTime;
+	    		    prevTime = time;
+	    		    sleepTime = prefferedTime - (int)timetaken;
+	    		    pressTimer --;
+	    		    spawnTimer --;
+	    	//to see the amount of time taken in each cycle
+	    		    //System.out.println(sleepTime);
+	    		    //System.out.println(prefferedTime);
+	    		    //System.out.println(timetaken);
+	    		    //System.out.println((int)timetaken+sleepTime);
+	    		    if (!((int)timetaken > prefferedTime)){
+	    		    	Thread.sleep(sleepTime);
+	    		    	//System.out.print("true");
+	    		    }
+	    		    
+	    		} catch(InterruptedException ex) {
+	    			System.out.println("InterruptedException");
+	    		}
+	    	}
+	    }.start();
+	    
+	    Window.show();
+	}
+	
+	public static void main(String[] args) {
+		 
+        launch(args);
+        
+        System.out.print(Objects.objectlist.size());
+        
+	}
+	
+}
---
+++ Objects.java
@@ -0,0 +1,297 @@
+package GameData;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import javafx.scene.image.Image;
+
+public class Objects extends Game{
+	public static ArrayList<Objects> objectlist = new ArrayList<Objects>();
+	private double x;
+	private double y;
+	private double MoveSpeed;
+	private Image face;
+	private int sizeX;
+	private int sizeY;
+	private boolean Touching = false;
+	private String name = "";
+	private double DownRandom = 0;
+	private double RightRandom = 0;
+	private Random random = new Random();
+	private double Xvelocity;
+	private double Yvelocity;
+
+	//right end of object
+	public double Right(){
+		double farx = x + sizeX - 1;
+		return (farx);
+	}
+	//bottom end of object
+	public double Bottom(){
+		double fary = y + sizeY - 1;
+		return (fary);
+	}
+	//current x of object
+	public double Left(){
+		return (x);
+	}
+	//current y of object
+	public double Top(){
+		return (y);
+	}
+	public double CenterX(){
+		return(x+sizeX/2);
+	}
+	public double CenterY(){
+		return(y+sizeY/2);
+	}
+	//changes the y of object
+	public void NewY(double y){
+		this.y = y;
+	}
+	//changes the x of object
+	public void NewX(double x){
+		this.x = x;
+	}
+	//returns the image of the object
+	public Image sprite(){
+		return (face);
+	}
+	public double MoveSpeed(){
+		return (MoveSpeed);
+	}
+	public void Sprite(Objects object, String picture){
+		object.face = new Image(picture);
+	}
+	public boolean Touching(){
+		return (Touching);
+	}
+	public void isTouching(boolean touch){
+		this.Touching = touch;
+	}
+	
+	public String Name(){
+		return (name);
+	}
+	public double Xspeed(){
+		return (Xvelocity);
+	}
+	public double Yspeed(){
+		return (Yvelocity);
+	}
+	
+	//creates the object
+	//square moving objects
+	
+	public Objects(double startx, double starty, double speed, String image, int xsize, int ysize, String name, double Xvelocity, double Yvelocity){
+		this.face = new Image(image);
+		this.x = startx;
+		this.y = starty;
+		this.MoveSpeed = speed;
+		this.sizeX = xsize;
+		this.sizeY = ysize;
+		this.name = name;
+		this.Xvelocity = Xvelocity;
+		this.Yvelocity = Yvelocity;
+		objectlist.add(this);
+	}
+	//square objects
+	
+	//health bar
+	
+	public Objects(String image){
+		this.face = new Image(image);
+	}
+
+	
+	public double[] Move(Objects moving, double moveX, double moveY) {
+		double[] movement = new double[2];
+		// MAKE SURE ONLY X OR Y IS INPUTED. NOT BOTH AT ONCE
+		//   ^ need to fix this eventually
+		//System.out.println(moveX + " " + moveY);
+		for (Objects item : objectlist) {
+			if (item == moving) // don't compare against self
+				continue;
+			if (moving.Right() + moveX < item.Left()){
+				continue;
+			}
+			if (moving.Left() + moveX > item.Right()){
+				continue;
+			}
+			if (moving.Bottom() + moveY < item.Top()){
+				continue;
+			}
+			if (moving.Top() + moveY > item.Bottom()){
+				continue;
+			}
+			// calculate amount that can be moved, return to caller
+			// also calculate if touching other objects
+			moving.Touching = false;
+			
+			if (moveX > 0){
+				moveX = Math.min(moveX, item.Left() - moving.Right()-1);
+				if (moveX == 0)
+					if (item.name == "bullet" && moving.name != "bullet"){
+						item.Touching = true;
+						moving.Touching = true;
+					} else if (item.name == "" && moving.name != "" && moving.name != "player"){
+						item.Touching = true;
+						moving.Touching = true;
+						
+					}
+			}
+			else if (moveX < 0){
+				moveX = -Math.min(-moveX, moving.Left() - item.Right()-1);
+				if (moveX == 0)
+					if (item.name == "bullet" && moving.name != "bullet"){
+						item.Touching = true;
+						moving.Touching = true;
+					} else if (item.name == "" && moving.name != "" && moving.name != "player"){
+						item.Touching = true;
+						moving.Touching = true;
+						
+					}
+			}
+			
+			if (moveY > 0) {
+				moveY = Math.min(moveY, item.Top() - moving.Bottom()-1);
+				if (moveY == 0)
+					if (item.name == "bullet" && moving.name != "bullet"){
+						item.Touching = true;
+						moving.Touching = true;
+					} else if (item.name == "" && moving.name != "" && moving.name != "player"){
+						item.Touching = true;
+						moving.Touching = true;
+						
+					}
+			}
+			else if (moveY < 0){
+				moveY = -Math.min(-moveY, moving.Top() - item.Bottom()-1);
+				if (moveY == 0)
+					if (item.name == "bullet" && moving.name != "bullet"){
+						item.Touching = true;
+						moving.Touching = true;
+					} else if (item.name == "" && moving.name != "" && moving.name != "player"){
+						item.Touching = true;
+						moving.Touching = true;
+						
+					}
+			}
+		}
+		
+		
+		//System.out.println("moving " + moveX + ", " + moveY);
+		//System.out.println(moveY);
+		moving.x += moveX;
+		moving.y += moveY;
+		movement[0] = moveX;
+		movement[1] = moveY;
+		return (movement);
+
+	}
+	
+	public void Follow(Objects follower, Objects leader){
+		double moveX = 0;
+		double moveY = 0;
+		double moved[] = { 0, 0 };
+
+		if (follower.RightRandom != 0 || follower.DownRandom != 0) {			
+			//System.out.println(follower.counter);
+			moveX = Math.copySign(Math.min(Math.abs(follower.RightRandom), follower.MoveSpeed),
+								  follower.RightRandom);
+			follower.RightRandom -= moveX;
+		
+			moveY = Math.copySign(Math.min(Math.abs(follower.DownRandom), follower.MoveSpeed),
+								  follower.DownRandom);
+			
+			follower.DownRandom -= moveY;
+		    
+		} else {
+			if (follower.CenterX() < leader.CenterX()) {
+				moveX = follower.MoveSpeed;
+			} else if (follower.CenterX() > leader.CenterX()) {
+				moveX = -follower.MoveSpeed;
+			} else {
+				moveX = 0;
+			}		
+			if (follower.CenterY() < leader.CenterY()) {
+				moveY = follower.MoveSpeed;
+			} else if (follower.CenterY() > leader.CenterY()) {
+				moveY = -follower.MoveSpeed;
+			} else {
+				moveY = 0;
+			}
+		}
+	    //System.out.println("move " + moveX + " " + moveY);
+
+		if (moveX != 0)
+			moved = follower.Move(follower, moveX, 0);
+		if (moveY != 0)
+			moved = follower.Move(follower, 0, moveY);
+		 
+		if (moved[0] == 0 && moved[1] == 0) {
+			
+			//System.out.println("stuck");
+			if (follower.DownRandom == 0 && follower.RightRandom == 0) {
+				follower.RightRandom = 6 * (follower.random.nextDouble() - 0.5) * follower.sizeX;
+				follower.DownRandom = 6 * (follower.random.nextDouble() - 0.5) * follower.sizeY;
+			    //System.out.println("select " + follower.RightRandom + " " + follower.DownRandom);
+
+			}
+		}
+	}	
+	
+	public void Follownew(Objects follower, Objects leader){
+
+		double MoveX = 0;
+		double MoveY = 0;
+		
+		if (follower.CenterX() < leader.CenterX())
+			MoveX = follower.MoveSpeed();
+		else if (follower.CenterX() > leader.CenterX())
+			MoveX = -follower.MoveSpeed();
+		else
+			MoveY = follower.MoveSpeed() / 2;
+		
+		if (follower.CenterY() < leader.CenterY())
+			MoveY += follower.MoveSpeed();
+		else if (follower.CenterY() > leader.CenterY())
+			MoveY += -follower.MoveSpeed();
+		else
+			MoveX += follower.MoveSpeed() / 2;
+		
+		follower.Move(follower, MoveX, MoveY);
+	}
+	
+	public void Shoot(Objects shooter, double Xdirection, double Ydirection, double speed){
+		double Xmove;
+		double Ymove;
+		double DistanceToTravel;
+		double x = 0;
+		double y = 0;
+		int BulletSize = 16;
+		
+		// also need to make the bullet appear on the side where its being shot from
+		Xmove = Xdirection - shooter.x;
+		Ymove = Ydirection - shooter.y;
+		//System.out.println(Xmove);
+		//System.out.println(Ymove);
+		if (Xmove > 0)
+			x = shooter.Right() + 1;
+		else
+			x = shooter.Left() - BulletSize;
+		if (Ymove > 0)
+			y = shooter.Bottom() + 1;
+		else
+			y = shooter.Top() - 1;
+		
+		DistanceToTravel = Math.sqrt((Xmove*Xmove)+(Ymove*Ymove));
+		double change = speed/DistanceToTravel;
+		double xvelocity = (Xmove*change*speed);
+		double yvelocity = (Ymove*change*speed);
+		AiList.add(new Objects(x, y, speed, "Bullet16x16.png", BulletSize, BulletSize, "bullet", xvelocity, yvelocity));
+		
+		
+	}
+	
+}
